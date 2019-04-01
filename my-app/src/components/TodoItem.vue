8afe735c679a76dca49cf78de6e425544177d435
Eric Lu <eric.lu02@sap.com>
2019-04-01 23:40:59 +0800

    Test vue-cli for initial an app

---
+++ my-app/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="item">
+    <input type="checkbox" v-model="checked">
+    <slot name="itemSlot" v-bind="{checked}"></slot>
+    <span>{{ testprop }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['testprop'],
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+    color: red;
+}
+</style>
